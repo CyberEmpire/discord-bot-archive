6908c6b588585382ce62f682dd085585966ef8e7
Caesarovich <cesar@caesarovich.xyz>
2021-09-20 01:23:30 +0200

    Did ban + kick commands

---
+++ src/commands/Administration/ban.ts
@@ -0,0 +1,61 @@
+import { Args, Command, PieceContext } from '@sapphire/framework';
+import { GuildMember, Message, MessageEmbed, User } from 'discord.js';
+
+async function banMember(member: GuildMember, reason: string) {
+	if (member.bannable) {
+		await member
+			.send(
+				`**You have been banned from ${member.guild.name}. Reason: ${reason}**`
+			)
+			.catch(() => {});
+		await member.ban({ reason, days: 1 });
+	}
+}
+
+function makeBanEmbed(
+	author: User,
+	member: GuildMember,
+	reason: string
+): MessageEmbed {
+	const message = member.bannable
+		? `**${member.user.tag} has been banned.**`
+		: `**${member.user.tag} couldn't be banned.**`;
+
+	const embed = new MessageEmbed()
+		.setColor(member.bannable ? 'RED' : 'AQUA')
+		.setAuthor(`banned by ${author.tag}`, author.displayAvatarURL())
+		.setTitle(message)
+		.setDescription(`**Reason:** ${reason}`)
+		.setThumbnail(member.user.displayAvatarURL())
+		.setTimestamp(Date.now());
+
+	return embed;
+}
+
+class PingCommand extends Command {
+	constructor(context: PieceContext) {
+		super(context, {
+			name: 'Ban',
+			description: 'Ban a member from your guild.',
+			detailedDescription: '{[Members]} (Reason)',
+			requiredClientPermissions: ['BAN_MEMBERS'],
+			requiredUserPermissions: ['BAN_MEMBERS'],
+		});
+	}
+
+	async run(message: Message, args: Args) {
+		const members = await args.repeat('member');
+		const reason = await args.rest('string').catch(() => 'No Reason');
+
+		const embeds: MessageEmbed[] = [];
+
+		for (const m of members) {
+			embeds.push(makeBanEmbed(message.author, m, reason));
+			await banMember(m, reason);
+		}
+
+		message.reply({ embeds });
+	}
+}
+
+export default PingCommand;
---
+++ src/commands/Administration/kick.ts
@@ -0,0 +1,61 @@
+import { Args, Command, PieceContext } from '@sapphire/framework';
+import { GuildMember, Message, MessageEmbed, User } from 'discord.js';
+
+async function kickMember(member: GuildMember, reason: string) {
+	if (member.kickable) {
+		await member
+			.send(
+				`**You have been kicked from ${member.guild.name}. Reason: ${reason}**`
+			)
+			.catch(() => {});
+		await member.kick(reason);
+	}
+}
+
+function makeKickEmbed(
+	author: User,
+	member: GuildMember,
+	reason: string
+): MessageEmbed {
+	const message = member.kickable
+		? `**${member.user.tag} has been kicked.**`
+		: `**${member.user.tag} couldn't be kicked.**`;
+
+	const embed = new MessageEmbed()
+		.setColor(member.kickable ? 'RED' : 'AQUA')
+		.setAuthor(`Kicked by ${author.tag}`, author.displayAvatarURL())
+		.setTitle(message)
+		.setDescription(`**Reason:** ${reason}`)
+		.setThumbnail(member.user.displayAvatarURL())
+		.setTimestamp(Date.now());
+
+	return embed;
+}
+
+class PingCommand extends Command {
+	constructor(context: PieceContext) {
+		super(context, {
+			name: 'Kick',
+			description: 'Kick a member from your guild.',
+			detailedDescription: '{[Members]} (Reason)',
+			requiredClientPermissions: ['KICK_MEMBERS'],
+			requiredUserPermissions: ['KICK_MEMBERS'],
+		});
+	}
+
+	async run(message: Message, args: Args) {
+		const members = await args.repeat('member');
+		const reason = await args.rest('string').catch(() => 'No Reason');
+
+		const embeds: MessageEmbed[] = [];
+
+		for (const m of members) {
+			embeds.push(makeKickEmbed(message.author, m, reason));
+			await kickMember(m, reason);
+		}
+
+		message.reply({ embeds });
+	}
+}
+
+export default PingCommand;
