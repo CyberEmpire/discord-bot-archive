b7cb97e19c1fac01789710268653564a91e89e18
Caesarovich <cesar@caesarovich.xyz>
2021-09-19 22:55:07 +0200

    Added advanced help features

--- src/commands/General/help.ts
+++ src/commands/General/help.ts
@@ -1,4 +1,4 @@
-import { Command, CommandStore, PieceContext } from '@sapphire/framework';
+import { Args, Command, CommandStore, PieceContext } from '@sapphire/framework';
 import { Message, MessageEmbed } from 'discord.js';
 
 function sortCommands(commands: CommandStore): Map<string, Array<Command>> {
@@ -30,6 +30,38 @@ function joinCommands(commands: Array<Command>): string {
 	return commandNames.join(', ');
 }
 
+function makeCommandList(commands: CommandStore): MessageEmbed {
+	const categories = sortCommands(commands);
+
+	const embed = new MessageEmbed()
+		.setTitle('Commands list')
+		.setDescription(
+			`This is a list of available commands. Commands you aren\'t authorised to use may not be visible to you. Use \`!help {Command}\` to get help about a specific command.`
+		)
+		.setColor('#00df11');
+
+	categories.forEach((v, k) => {
+		embed.addField(k, joinCommands(v));
+	});
+
+	return embed;
+}
+
+function makeHelpEmbed(command: Command): MessageEmbed {
+	const embed = new MessageEmbed()
+		.setAuthor(command.category ?? 'No category')
+		.setTitle(command.name)
+		.setDescription(command.description)
+		.setColor('#00df11');
+
+	if (command.aliases) {
+		const aliases = command.aliases.map((a) => `\`${a}\``);
+		embed.addField('Aliases', aliases.join(', '));
+	}
+
+	return embed;
+}
+
 class HelpCommand extends Command {
 	constructor(context: PieceContext) {
 		super(context, {
@@ -39,23 +71,21 @@ class HelpCommand extends Command {
 		});
 	}
 
-	async run(message: Message) {
+	async run(message: Message, args: Args) {
 		const commands = this.container.stores.get('commands');
+		let arg = args.finished ? null : await args.pick('string');
 
-		const categories = sortCommands(commands);
+		if (arg) {
+			const cmd = commands.get(arg);
 
-		const embed = new MessageEmbed()
-			.setTitle('Commands list')
-			.setDescription(
-				`This is a list of available commands. Commands you aren\'t authorised to use may not be visible to you. Use \`!help {Command}\` to get help about a specific command.`
-			)
-			.setColor('#00df11');
-
-		categories.forEach((v, k) => {
-			embed.addField(k, joinCommands(v));
-		});
-
-		message.reply({ embeds: [embed] });
+			if (cmd) {
+				message.reply({ embeds: [makeHelpEmbed(cmd)] });
+			} else {
+				message.reply(`❓ The \`${arg}\` command doesn't exist.`);
+			}
+		} else {
+			message.reply({ embeds: [makeCommandList(commands)] });
+		}
 	}
 }
 
