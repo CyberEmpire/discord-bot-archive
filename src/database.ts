37a0f8c684e271cc549c5566c4fd437563dbc9dd
Caesarovich <cesar@caesarovich.xyz>
2021-09-22 02:55:31 +0200

    Added basic database instance

---
+++ src/database.ts
@@ -0,0 +1,29 @@
+import { Sequelize, Options } from 'sequelize';
+import { yellow } from 'chalk';
+import { container } from '@sapphire/framework';
+
+export class Database extends Sequelize {
+	public setup(): Promise<void> {
+		return new Promise(async (resolve, reject) => {
+			await super.authenticate().catch(reject);
+
+			container.client.logger.info(
+				`Connected to database ${yellow(this.config.username)}@${yellow(this.config.host)}:${yellow(
+					this.config.database
+				)}`
+			);
+
+			resolve();
+		});
+	}
+
+	constructor(config: Options) {
+		super(config);
+	}
+}
+
+declare module '@sapphire/pieces' {
+	interface Container {
+		database: Database;
+	}
+}
