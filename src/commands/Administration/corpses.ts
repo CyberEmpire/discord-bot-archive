ef0f793b238091dd29a0f28a8c740f4ea149dac1
AzeS <60826595+XxAzeSxX@users.noreply.github.com>
2021-10-13 22:56:37 +0200

    Create corpses.ts

---
+++ src/commands/Administration/corpses.ts
@@ -0,0 +1,12 @@
+import { Command, PieceContext } from '@sapphire/framework';
+import type { MemberLevel } from '../../modules/leveling';
+import { GuildMember, Message, MessageEmbed, User } from 'discord.js';
+
+
+	async run(message: Message) {
+		//if User.level = admin
+    // corpses.foreach() {
+    // message.reply(`🎲 You rolled a **${num}** !`);
+    // }
+	}
+}
